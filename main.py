0a5037b2e672356a32d9b9cea6f0ddf5ece7e7b9
Grovally <grovallypvtltd@gmail.com>
2026-08-17 16:20:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
         "http://localhost:5173",
-       
+        "http://127.0.0.1:5173",
+        "https://chhabra-properties.com",
+        "https://www.chhabra-properties.com",
     ],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
 )
 
